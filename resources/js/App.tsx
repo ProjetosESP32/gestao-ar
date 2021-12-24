506846adc48cebc6db45c0501cac134229024d9f
gabirum <gabrielmc15@outlook.com>
2021-12-23 20:36:48 -0400

    configuração do frontend

--- resources/js/App.tsx
+++ resources/js/App.tsx
@@ -3,6 +3,8 @@ import { CssBaseline } from '@mui/material'
 import React from 'react'
 import { render } from 'react-dom'
 
+import '../css/global.css'
+
 createInertiaApp({
   resolve: name => require(`./pages/${name}`),
   setup({ el, App, props }) {
