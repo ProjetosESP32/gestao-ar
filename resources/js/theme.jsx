d5a06b0f39f4ed3afd94d2418902b49c860ae8bf
joao <joao.campos@unemat.br>
2021-12-31 19:33:17 -0400

    move as configuraçoes do tema para um arquivo theme.js

---
+++ resources/js/theme.jsx
@@ -0,0 +1,46 @@
+import { createTheme } from '@mui/material/styles'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#002984',
+    },
+  },
+
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: 32,
+          width: '100%',
+          paddingTop: 16,
+          paddingBottom: 16,
+        },
+      },
+    },
+
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          borderRadius: 32,
+        },
+      },
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: {
+          marginLeft: 8,
+        },
+      },
+    },
+    MuiFormControl: {
+      styleOverrides: {
+        root: {
+          width: '100%',
+        },
+      },
+    },
+  },
+})
+
+export { theme }
