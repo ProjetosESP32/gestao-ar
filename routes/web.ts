ec1bd2eb7fa9d1a66e5a88fec8b18d6b0b70b328
joao <joao.campos@unemat.br>
2021-12-30 21:04:56 -0400

    configura rota '' para a tela de login

--- routes/web.ts
+++ routes/web.ts
@@ -2,4 +2,5 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
   Route.get('user', ({ inertia }) => inertia.render('User/Index'))
+  Route.get('', ({ inertia }) => inertia.render('Login/Index'))
 }).middleware('inertia')
