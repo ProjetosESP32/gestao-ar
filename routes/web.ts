64b05ff3344cf09568247e06325b8c8ff055a4da
joao <joao.campos@unemat.br>
2021-12-31 22:02:09 -0400

    adiciona rota para a pagina de alterar senha

--- routes/web.ts
+++ routes/web.ts
@@ -3,4 +3,5 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.group(() => {
   Route.get('user', ({ inertia }) => inertia.render('User/Index'))
   Route.get('', ({ inertia }) => inertia.render('Login/Index'))
+  Route.get('/alterar-senha', ({ inertia }) => inertia.render('ChangePassword/Index'))
 }).middleware('inertia')
