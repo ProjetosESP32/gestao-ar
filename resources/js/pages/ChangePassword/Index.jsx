268d939c7c300fcc9db836f8ac7f9fd3d31ebbaf
joao <joao.campos@unemat.br>
2022-01-01 20:01:55 -0400

    Organiza pagina de alterar senha  reutilizando a mesma estrutura da pagina de login

--- resources/js/pages/ChangePassword/Index.jsx
+++ resources/js/pages/ChangePassword/Index.jsx
@@ -1,93 +1,70 @@
 import { ThemeProvider, Box, Button, TextField, Grid, RadioGroup, Radio, FormControlLabel } from '@mui/material'
 import React from 'react'
 import '../Login/styles.css'
+import './style.css'
 import { theme } from '../../theme.jsx'
 
-function Login() {
+function ChangePassword() {
   return (
     <ThemeProvider theme={theme}>
       <div className={'root'}>
-        <img src={'/images/login-wave.svg'} className={'loginWave'} />
-        <div className={'loginContainer'}>
-          <Grid container columns={{ xs: 1, md: 12, lg: 12, xl: 12 }}>
-            <Grid
-              item
-              md={7}
-              xs={0}
-              xl={8}
-              style={{
-                backgroundImage: `url('/images/login-img.svg')`,
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: 'contain',
-                backgroundPosition: 'center',
-              }}
-            ></Grid>
-            <Grid item md={5} xs={1} xl={4} style={{ padding: '2rem 0' }}>
-              <Grid container spacing={2} columns={{ xs: 1, lg: 1 }}>
-                <Grid item xs={1} lg={1}>
-                  <h2 className={'loginTitle'}>Login</h2>
-                </Grid>
-                <Grid item xs={1} lg={1}>
-                  <div className={'loginIconGroup'}>
-                    <Button className={'loginIconButton'} variant={'outlined'}>
-                      <img
-                        style={{
-                          width: '80%',
-                          display: 'block',
-                          margin: 'auto',
-                        }}
-                        src={'/images/apple-icon.svg'}
-                      />
-                    </Button>
-                    <Button className={'loginIconButton'} variant={'outlined'}>
-                      <img
-                        style={{
-                          width: '80%',
-                          display: 'block',
-                          margin: 'auto',
-                        }}
-                        src={'/images/google-icon.svg'}
-                      />
-                    </Button>
-                  </div>
+        <img src={'/images/change-pass-wave.svg'} className={'changePassWave'} />
+
+        <Grid spacing={1} container columns={{ xl: 12, lg: 12 }} justifyContent={'center'}>
+          <Grid item xl={7} lg={8}>
+            <div className={'loginContainer'} style={{ paddingTop: '6vh', paddingBottom: '11vh' }}>
+              <Grid spacing={4} container columns={{ xs: 1, md: 12, lg: 12, xl: 12 }} justifyContent={'space-evenly'}>
+                <Grid
+                  rowSpacing={1}
+                  item
+                  md={6}
+                  xs={0}
+                  xl={6}
+                  lg={6}
+                  style={{
+                    backgroundImage: `url('/images/change-pass-img.svg')`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'contain',
+                    backgroundPosition: 'center',
+                  }}
+                ></Grid>
+                <Grid item md={6} xs={1} xl={5}>
+                  <Grid container spacing={2} columns={{ xs: 1, lg: 1 }}>
+                    <Grid item xs={1} lg={1}>
+                      <h2 className={'resetPassTitle'}>Informe Sua Nova Senha!</h2>
+                    </Grid>
+                  </Grid>
+
+                  <Grid
+                    container
+                    spacing={3}
+                    columns={{ xs: 12, md: 12 }}
+                    alignItems={'center'}
+                    justifyContent={'center'}
+                  >
+                    <Grid item xs={11} md={11}>
+                      <p className={'resetPassText'}>Por favor, preencha os campos abaixo com a nova senha.</p>
+                    </Grid>
+                    <Grid item xs={11} md={11}>
+                      <TextField style={{ borderRadius: '8' }} type={'password'} label={'Senha'} />
+                    </Grid>
+                    <Grid item xs={11} md={11}>
+                      <TextField style={{ borderRadius: '8' }} type={'password'} label={'Nova Senha'} />
+                    </Grid>
+                    <Grid item xs={11} md={11}>
+                      <Button variant={'contained'} style={{ borderRadius: '8' }}>
+                        Confirmmar
+                      </Button>
+                    </Grid>
+                  </Grid>
                 </Grid>
               </Grid>
-
-              <Grid container spacing={3} columns={{ xs: 12, md: 12 }} alignItems={'center'} justifyContent={'center'}>
-                <Grid item xs={11} md={11}>
-                  <div className={'login-dividing-line'}>
-                    <div></div>
-                    <p>Ou</p>
-                    <div></div>
-                  </div>
-                </Grid>
-                <Grid item xs={11} md={11}>
-                  <TextField label={'Email'} />
-                </Grid>
-                <Grid item xs={11} md={11}>
-                  <TextField style={{ borderRadius: '8' }} type={'password'} label={'Senha'} />
-                </Grid>
-
-                <Grid item xs={11} md={11}>
-                  <RadioGroup name={'controlled-radio-buttons-group'}>
-                    <FormControlLabel value={'female'} control={<Radio />} label={'Lembrar usuário'} />
-                  </RadioGroup>
-                </Grid>
-                <Grid item xs={11} md={11}>
-                  <Button variant={'contained'} style={{ borderRadius: '8' }}>
-                    Entrar
-                  </Button>
-                </Grid>
-                <Grid item xs={11} md={11} justifyContent={'end'}>
-                  <p className={'forgot-password'}>Esqueceu a senha</p>
-                </Grid>
-              </Grid>
-            </Grid>
+            </div>
           </Grid>
-        </div>
+        </Grid>
       </div>
     </ThemeProvider>
   )
 }
 
-export default Login
+export default ChangePassword
