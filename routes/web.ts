6abbcc38fc8a0256d11b072a641b16c06a0c9bc3
joao <joao.campos@unemat.br>
2022-01-02 21:29:31 -0400

    adiciona rota para a pagina de recuperar senha

--- routes/web.ts
+++ routes/web.ts
@@ -4,4 +4,5 @@ Route.group(() => {
   Route.get('user', ({ inertia }) => inertia.render('User/Index'))
   Route.get('', ({ inertia }) => inertia.render('Login/Index'))
   Route.get('/alterar-senha', ({ inertia }) => inertia.render('ChangePassword/Index'))
+  Route.get('/recuperar-senha', ({ inertia }) => inertia.render('RecoverPassword/Index'))
 }).middleware('inertia')
