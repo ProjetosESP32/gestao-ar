d0665f12c7fec07fab0e3bf631f4aa1afb93df28
joao <joao.campos@unemat.br>
2022-01-28 20:44:23 -0400

    adiciona a pagina de controle de sala

--- routes/web.ts
+++ routes/web.ts
@@ -2,7 +2,9 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
   Route.get('user', ({ inertia }) => inertia.render('User/Index'))
-  Route.get('', ({ inertia }) => inertia.render('Login/Index'))
-  Route.get('/alterar-senha', ({ inertia }) => inertia.render('ChangePassword/Index'))
-  Route.get('/recuperar-senha', ({ inertia }) => inertia.render('RecoverPassword/Index'))
+  Route.get('login', ({ inertia }) => inertia.render('Login/Index'))
+  Route.get('alterar-senha', ({ inertia }) => inertia.render('ChangePassword/Index'))
+  Route.get('recuperar-senha', ({ inertia }) => inertia.render('RecoverPassword/Index'))
+  Route.get('inicio', ({ inertia }) => inertia.render('Home/Index'))
+  Route.get('controle-de-sala', ({ inertia }) => inertia.render('RoomControl/Index'))
 }).middleware('inertia')
