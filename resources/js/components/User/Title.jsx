52e154c3b09576136d975b209d1ad212bce98c72
joao <joao.campos@unemat.br>
2022-01-30 18:21:35 -0400

    cria componente para o titulo das campos de usuario

---
+++ resources/js/components/User/Title.jsx
@@ -0,0 +1,13 @@
+import { styled } from '@mui/material/styles'
+import Typography from '@mui/material/Typography'
+
+const UserTitle = styled(Typography)({
+  color: '#5D99C6',
+  fontSize: '1.5rem',
+  fontWeight: 'bolder',
+  textAlign: 'left',
+  display: 'block',
+  width: '100%',
+})
+
+export default UserTitle
