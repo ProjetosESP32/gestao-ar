6eed27362e47384131d1cc9c489addd5d5bb259a
joao <joao.campos@unemat.br>
2022-01-29 22:29:30 -0400

    adiciona tela de conta de usuario ainda sem a foto de perfil

---
+++ resources/js/components/User/TextField.jsx
@@ -0,0 +1,29 @@
+import { styled } from '@mui/material/styles'
+import TextField from '@mui/material/TextField'
+
+const AccountTextField = styled(TextField)(({ theme }) => ({
+  ...theme.typography.body2,
+  [`& fieldset`]: {
+    borderRadius: 8,
+    borderColor: '#90CAF9',
+  },
+  [`& input`]: {
+    '&::placeholder': {
+      color: '#90CAF9',
+    },
+  },
+  'marginTop': theme.spacing(2),
+  'marginBottom': theme.spacing(2),
+  'width': '100%',
+  'display': 'flex',
+  '& label.Mui-focused': {
+    color: '#90CAF9',
+  },
+  '& .MuiOutlinedInput-root': {
+    '&.Mui-focused fieldset': {
+      borderColor: '#90CAF9',
+    },
+  },
+}))
+
+export default AccountTextField
