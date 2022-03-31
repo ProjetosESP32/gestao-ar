2ed9fef85b49f69b80fba5a7ebc056d04316457a
joao <joao.campos@unemat.br>
2022-03-31 09:24:50 -0400

    adiciona novo componente no arquivo Buttons

--- resources/js/components/Icon/Index.jsx
+++ resources/js/components/Icon/Index.jsx
@@ -2,8 +2,8 @@ import * as PropTypes from 'prop-types'
 import React from 'react'
 import './styles.css'
 
-export default function Icon({ name: Name }) {
-  return <Name className='MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-i4bv87-MuiSvgIcon-root' />
+export default function Icon({ name: Name, ...rest }) {
+  return <Name className='MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-i4bv87-MuiSvgIcon-root' {...rest} />
 }
 
 Icon.propTypes = {
