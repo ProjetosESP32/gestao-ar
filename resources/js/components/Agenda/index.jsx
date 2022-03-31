51a09c3b3a893f3c8e719283c8413f1241b1f0c3
joao <joao.campos@unemat.br>
2022-03-31 09:23:53 -0400

    adiciona componente de agenda

--- resources/js/components/Agenda/index.jsx
+++ resources/js/components/Agenda/index.jsx
@@ -1,59 +1,120 @@
+import { Grid, IconButton } from '@mui/material'
 import Box from '@mui/material/Box'
 import Modal from '@mui/material/Modal'
 import Step from '@mui/material/Step'
 import StepConnector, { stepConnectorClasses } from '@mui/material/StepConnector'
 import StepLabel from '@mui/material/StepLabel'
 import Stepper from '@mui/material/Stepper'
-import TextField from '@mui/material/TextField'
-import Typography from '@mui/material/Typography'
-import { styled } from '@mui/material/styles'
-import PropTypes, { node } from 'prop-types'
 
+import { styled } from '@mui/material/styles'
+import { node } from 'prop-types'
 import * as React from 'react'
-import { MdRequestQuote } from 'react-icons/md'
+import { MdSearch, MdPowerSettingsNew, MdBuild } from 'react-icons/md'
+import { AccountButton, ScheduleButton } from '../../components/User/Buttons'
+import { ControlInput, ControlLabel, ControlSelect, ControlTextArea } from '../../components/User/TextField.jsx'
+import UserTitle from '../../components/User/Title.jsx'
+import Icon from '../Icon/Index'
+
+const style = {
+  position: 'absolute',
+  top: '40%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  backgroundColor: 'white',
+  border: 'none',
+  boxShadow: '0 3px 6px #00000049',
+  padding: '0.75rem 2.5rem',
+  '&:focus': {
+    border: 'none!important',
+  },
+}
+
+const StyledModal = styled(Modal)(() => ({
+  // backgroundColor: 'red',
+  [`& div.MuiBackdrop-root`]: {
+    backgroundColor: 'rgba(0,0,0,0)',
+  },
+}))
+
+const StyledControlSelect = styled(ControlSelect)(() => ({
+  [`& option`]: {
+    color: 'black',
+    fontWeight: 'bold',
+  },
+}))
 
 const Agenda = () => {
-  const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: 'none!important',
-    boxShadow: 24,
-    p: 4,
-  }
   const [open, setOpen] = React.useState(false)
   const handleOpen = () => setOpen(true)
   const handleClose = () => setOpen(false)
 
   return (
     <Box sx={{ width: '100%' }}>
-      <Modal
+      <StyledModal
         open={open}
         onClose={handleClose}
         aria-labelledby='modal-modal-title'
         aria-describedby='modal-modal-description'
       >
-        <Box sx={style}>
-          <Typography id='modal-modal-title' variant='h6' component='h2'>
-            Text in a modal
-          </Typography>
-          <Typography id='modal-modal-description' sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+        <Box style={style}>
+          <UserTitle style={{ margin: '1.5rem 0' }} variant='p'>
+            Evento
+          </UserTitle>
+
+          <Grid container justifyContent='space-between' columns={11} alignItems='center'>
+            <Grid item xs={5}>
+              <ControlLabel>Data</ControlLabel>
+              <ControlInput type='date' />
+            </Grid>
+            <Grid item xs={5}>
+              <ControlLabel>Ação</ControlLabel>
+              <StyledControlSelect>
+                <option>Ligar</option>
+                <option>Desligar</option>
+                <option>Manutenção</option>
+              </StyledControlSelect>
+            </Grid>
+            <Grid item xs={5}>
+              <ControlLabel>Início</ControlLabel>
+              <StyledControlSelect>
+                <option>00 am</option>
+                <option>01 am</option>
+                <option>02 am</option>
+                <option>03 am</option>
+              </StyledControlSelect>
+            </Grid>
+            <Grid item xs={5}>
+              <ControlLabel>Fim</ControlLabel>
+              <StyledControlSelect>
+                <option>00 am</option>
+                <option>01 am</option>
+                <option>02 am</option>
+                <option>03 am</option>
+              </StyledControlSelect>
+            </Grid>
+
+            <Grid item xs={11}>
+              <div style={{ marginTop: '2rem', display: 'flex', justifyContent: 'center' }}>
+                <AccountButton>Confirmar</AccountButton>
+              </div>
+            </Grid>
+          </Grid>
         </Box>
-      </Modal>
+      </StyledModal>
       <div
         style={{
           width: '100%',
           display: 'flex',
-          justifyContent: 'flex-start',
+          justifyContent: 'space-between',
           marginBottom: '2rem',
         }}
       >
-        <StyledDate type='date' />
+        <div style={{ display: 'flex' }}>
+          <StyledDate type='date' />
+          <Icon name={MdSearch} style={{ width: 32, height: 32, marginLeft: '0.5rem', fill: '#3F51B5' }}></Icon>
+        </div>
+        <ScheduleButton onClick={handleOpen}>+ Agendar</ScheduleButton>
       </div>
       <StyledStepperGroup>
         <Stepper activeStep={1} alternativeLabel connector={<InvisibleConector />}>
@@ -119,9 +180,9 @@ const StyledDate = styled('input')(() => ({
 
 function ColorlibStepIcon(props) {
   const icons = {
-    1: <MdRequestQuote />,
-    2: <MdRequestQuote />,
-    3: <MdRequestQuote />,
+    4: <MdPowerSettingsNew style={{ fill: '#FF0000' }} />,
+    2: <MdPowerSettingsNew style={{ fill: '#90CAF9' }} />,
+    3: <MdBuild style={{ fill: '#002984' }} />,
   }
 
   return <ColorlibStepIconRoot>{icons[String(props.icon)]}</ColorlibStepIconRoot>
