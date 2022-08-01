04c7e72e50654c90d032548155b4fd4b9b4731f4
Gabriel Mariano Costa <gabrielmc15@outlook.com>
2022-08-01 18:14:56 -0400

    refatoração das rotas

--- routes/web.ts
+++ routes/web.ts
@@ -2,7 +2,15 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
   Route.get('/', 'HomeController.index').as('home')
+  Route.get('auth/google/callback', 'GoogleAuthController.store').as('google.callback')
+  Route.get('users/verify-email/:email', 'UsersController.verifyEmail').mustBeSigned().as('users.verifyEmail')
+  Route.get('rooms', 'RoomsController.index')
+  Route.get('rooms/control/:id', 'RoomControlsController.show')
+})
+  .namespace('App/Controllers/Http/Web')
+  .middleware(['inertia', 'throttle:global'])
 
+Route.group(() => {
   Route.group(() => {
     Route.get('login', 'AuthLoginController.create').as('login.create')
     Route.post('login', 'AuthLoginController.store').as('login')
@@ -12,56 +20,47 @@ Route.group(() => {
   })
     .prefix('auth')
     .as('auth')
-    .middleware('guest')
-
-  Route.get('auth/google/callback', 'GoogleAuthController.store').as('google.callback')
 
   Route.group(() => {
-    Route.get('verify-email/:email', 'UsersController.verifyEmail').mustBeSigned().as('users.verifyEmail')
-
     Route.get('recover-password', 'UserRecoveriesController.create')
     Route.post('recover-password', 'UserRecoveriesController.store')
 
     Route.get('change-password/:email', 'UserRecoveriesController.edit').mustBeSigned().as('users.changePassword')
     Route.post('change-password/:email', 'UserRecoveriesController.update').mustBeSigned()
   }).prefix('users')
-
-  Route.group(() => {
-    Route.inertia('gerir-notificacoes', 'Control/NotificationControl')
-  }).prefix('control')
-
-  Route.resource('rooms', 'RoomsController').only(['index'])
-  Route.resource('rooms/control', 'RoomControlsController').only(['show'])
-
-  Route.group(() => {
-    Route.delete('logout', 'AuthLoginController.destroy').prefix('auth').as('auth.logout')
-
-    Route.group(() => {
-      Route.get('me', 'UsersController.show').as('users.profile')
-      Route.put('/', 'UsersController.update')
-      Route.patch('password', 'UsersController.updatePassword')
-      Route.delete('/photo', 'UsersController.deletePhoto')
-      Route.get('link-google', 'UsersController.linkGoogleAccount')
-    }).prefix('users')
-
-    Route.group(() => {
-      Route.resource('users', 'UsersController').except(['edit'])
-      Route.delete('users/:id/detach-room/:roomId', 'UsersController.detachRoom').as('users.detachRoom')
-      Route.resource('rooms', 'RoomsController').only(['store', 'update', 'destroy'])
-      Route.resource('apis', 'ServiceApiKeysController').except(['create', 'show', 'edit'])
-      Route.resource('esps', 'EspsController').only(['index', 'update', 'destroy'])
-    })
-      .namespace('App/Controllers/Http/Web/Admin')
-      .prefix('admin')
-      .as('admin')
-  }).middleware(['auth:web'])
 })
   .namespace('App/Controllers/Http/Web')
-  .middleware(['inertia'])
+  .middleware(['inertia', 'guest', 'throttle:global'])
+
+Route.group(() => {
+  Route.delete('logout', 'AuthLoginController.destroy').prefix('auth').as('auth.logout')
+
+  Route.group(() => {
+    Route.get('me', 'UsersController.show').as('users.profile')
+    Route.put('/', 'UsersController.update')
+    Route.patch('password', 'UsersController.updatePassword')
+    Route.delete('photo', 'UsersController.deletePhoto')
+    Route.get('link-google', 'UsersController.linkGoogleAccount')
+  }).prefix('users')
+})
+  .namespace('App/Controllers/Http/Web')
+  .middleware(['inertia', 'auth:web', 'throttle:global'])
 
 Route.group(() => {
   Route.post('rooms/control/:id/temperature', 'RoomControlsController.changeTemperature')
   Route.post('rooms/control/:id/power', 'RoomControlsController.changePower')
 })
   .namespace('App/Controllers/Http/Web')
-  .middleware(['auth:web'])
+  .middleware(['auth:web', 'throttle:global'])
+
+Route.group(() => {
+  Route.resource('users', 'UsersController').except(['edit'])
+  Route.delete('users/:id/detach-room/:roomId', 'UsersController.detachRoom').as('users.detachRoom')
+  Route.resource('rooms', 'RoomsController').only(['store', 'update', 'destroy'])
+  Route.resource('apis', 'ServiceApiKeysController').except(['create', 'show', 'edit'])
+  Route.resource('esps', 'EspsController').only(['index', 'update', 'destroy'])
+})
+  .namespace('App/Controllers/Http/Web/Admin')
+  .middleware(['inertia', 'auth:web', 'throttle:global'])
+  .prefix('admin')
+  .as('admin')
