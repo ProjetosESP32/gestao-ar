429d2c49d3b4b15f117ca3f20feef8432da99c2f
Gabriel Mariano Costa <gabrielmc15@outlook.com>
2022-08-01 09:09:14 -0400

    correção de tipos

--- resources/js/components/Drawer/withDrawer.tsx
+++ resources/js/components/Drawer/withDrawer.tsx
@@ -1,8 +1,7 @@
 import React, { FC, ComponentType } from 'react'
 import { Drawer } from './index'
 
-export function withDrawer<T>(component: ComponentType<T>): FC<T> {
-  const Component = component as any
+export function withDrawer<T extends JSX.IntrinsicAttributes>(Component: ComponentType<T>): FC<T> {
   return props => (
     <Drawer>
       <Component {...props} />
