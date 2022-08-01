f3ddcdea313895fe38ddfd415a4cb5043dcb09e8
Gabriel Mariano Costa <gabrielmc15@outlook.com>
2022-08-01 10:57:12 -0400

    scheduler com o bull

--- contracts/container.ts
+++
@@ -1,7 +0,0 @@
-declare module '@ioc:Adonis/Core/Application' {
-  import { SchedulerContract } from '@ioc:App/Scheduler'
-
-  export interface ContainerBindings {
-    'App/Scheduler': SchedulerContract
-  }
-}
--- start/schedule.ts
+++ start/schedule.ts
@@ -1,61 +1,66 @@
-import Event from '@ioc:Adonis/Core/Event'
-import type { EventsList } from '@ioc:Adonis/Core/Event'
+import Event, { EventsList } from '@ioc:Adonis/Core/Event'
 import Database from '@ioc:Adonis/Lucid/Database'
-import Scheduler from '@ioc:App/Scheduler'
+import Queue from 'bull'
 import { DateTime } from 'luxon'
-import ms from 'ms'
 import Room from 'App/Models/Room'
+import redis from 'Config/redis'
 
-Scheduler.registerTask(
-  'updateAirState',
-  async () => {
-    const client = await Database.transaction()
+const queue = new Queue('air-state', {
+  redis,
+})
 
-    const now = DateTime.now()
-    const nowSQL = now.toSQL()
-    const nowTime = now.toFormat('HH:mm:ss')
-    const rooms = await Room.query({ client })
-      .select('rooms.*')
-      .innerJoin('events', 'rooms.id', 'events.room_id')
-      .innerJoin('events_recurrences', 'events.id', 'events_recurrences.event_id')
-      .where('events.start_date', '<', nowSQL)
-      .where('events.end_date', '>', nowSQL)
-      .where('events_recurrences.days_of_week', 'LIKE', `%${now.weekday}%`)
-      .where('events_recurrences.days_of_month', 'LIKE', `%${now.day}%`)
-      .where('events_recurrences.start_time', '<', nowTime)
-      .where('events_recurrences.end_time', '>', nowTime)
-      .preload('events', eventBuilder =>
-        eventBuilder
-          .where('startDate', '<', nowSQL)
-          .where('endDate', '>', nowSQL)
-          .preload('eventRecurrences', eventRecurrenceBuilder =>
-            eventRecurrenceBuilder
-              .where('daysOfWeek', 'LIKE', `%${now.weekday}%`)
-              .where('daysOfMonth', 'LIKE', `%${now.day}%`)
-              .where('startTime', '<', nowTime)
-              .where('endTime', '>', nowTime),
-          ),
-      )
-      .preload('esps')
+queue.process(async _ => {
+  const client = await Database.transaction()
 
-    const eventToEmit = rooms
-      .map(room => {
-        const [event] = room.events
-        const recurrency = event.eventRecurrences.length
-        const isActive = room.esps.some(({ isOn }) => isOn)
+  const now = DateTime.now()
+  const nowSQL = now.toSQL()
+  const nowTime = now.toFormat('HH:mm:ss')
+  const rooms = await Room.query({ client })
+    .select('rooms.*')
+    .innerJoin('events', 'rooms.id', 'events.room_id')
+    .innerJoin('events_recurrences', 'events.id', 'events_recurrences.event_id')
+    .where('events.start_date', '<', nowSQL)
+    .where('events.end_date', '>', nowSQL)
+    .where('events_recurrences.days_of_week', 'LIKE', `%${now.weekday}%`)
+    .where('events_recurrences.days_of_month', 'LIKE', `%${now.day}%`)
+    .where('events_recurrences.start_time', '<', nowTime)
+    .where('events_recurrences.end_time', '>', nowTime)
+    .preload('events', eventBuilder =>
+      eventBuilder
+        .where('startDate', '<', nowSQL)
+        .where('endDate', '>', nowSQL)
+        .preload('eventRecurrences', eventRecurrenceBuilder =>
+          eventRecurrenceBuilder
+            .where('daysOfWeek', 'LIKE', `%${now.weekday}%`)
+            .where('daysOfMonth', 'LIKE', `%${now.day}%`)
+            .where('startTime', '<', nowTime)
+            .where('endTime', '>', nowTime),
+        ),
+    )
+    .preload('esps')
 
-        if (isActive && recurrency === 0) {
-          return { esps: room.esps, data: 0 }
-        }
+  const eventToEmit = rooms
+    .map(room => {
+      const [event] = room.events
+      const recurrency = event.eventRecurrences.length
+      const isActive = room.esps.some(({ isOn }) => isOn)
 
-        if (!isActive && recurrency > 0) {
-          return { esps: room.esps, data: 1 }
-        }
-      })
-      .filter(Boolean) as EventsList['air-change:dispatchAll']
+      if (isActive && recurrency === 0) {
+        return { esps: room.esps, data: 0 }
+      }
 
-    await Event.emit('air-change:dispatchAll', eventToEmit)
-    await client.commit()
+      if (!isActive && recurrency > 0) {
+        return { esps: room.esps, data: 1 }
+      }
+    })
+    .filter(Boolean) as EventsList['air-change:dispatchAll']
+
+  await Event.emit('air-change:dispatchAll', eventToEmit)
+  await client.commit()
+})
+
+queue.add(undefined, {
+  repeat: {
+    cron: '*/15 * * * *',
   },
-  ms('30m'),
-)
+})
