a5fb221bb8bfc9f28ada155ef744924fab8f05c3
Gabriel Mariano Costa <gabrielmc15@outlook.com>
2022-08-09 15:05:21 -0400

    pm2 e build

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: 'web-app',
+      script: './server.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      autorestart: true,
+      ignore_watch: ['node_modules'],
+    },
+  ],
+}
